83435c0ac3a6b5123a1d3058179b3014781c2e9c
Anaiah Kyrie Pinalba <mylearningarchive006@gmail.com>
2026-05-15 22:41:28 +0800

    Add enlarged certificate preview modal

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ const mobileMenu = document.getElementById("mobileMenu");
 menuBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
+
+const imageModal = document.getElementById("imageModal");
+const modalImage = document.getElementById("modalImage");
+
+window.openModal = function(imageSrc) {
+  imageModal.classList.remove("hidden");
+  imageModal.classList.add("flex");
+  modalImage.src = imageSrc;
+}
+
+window.closeModal = function() {
+  imageModal.classList.add("hidden");
+  imageModal.classList.remove("flex");
+}
